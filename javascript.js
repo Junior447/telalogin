88265807f2bc4afca33bd7fbf24b0844a709b606
Junior447 <jrraiol@hotmail.com>
2026-09-16 13:21:03 +0000

    Refactor contatoWhatsapp method to use class property
    Fix the contatoWhatsapp method to remove parameter and use class property correctly.

--- javascript.js
+++ javascript.js
@@ -21,7 +21,7 @@ class Cadastro {
       this.mnsErro = mnsErro;
       this.whatsapp = whatsapp;
       this.btn = btn;
-      this.#contato = this.#contato;
+      
       
 
    }
@@ -32,7 +32,7 @@ class Cadastro {
 
 
    }
-   contatoWhatsapp(contato) {
+   contatoWhatsapp() {
       this.whatsapp.addEventListener('click', (e) => {
          e.preventDefault()
 
@@ -50,9 +50,9 @@ class Cadastro {
 
 
             const mensagem = `Olá tudo bem meu nome é ${this.nome.value} 
-         vi seu número ${this.#contato} e tenho ${this.idade.value}, meu estado civil é ${this.estadocivil.value}
+         vi seu número ${#contato} e tenho ${this.idade.value}, meu estado civil é ${this.estadocivil.value}
          `
-            const url = `https://wa.me/${this.#contato}?text=${encodeURIComponent(mensagem)}`;
+            const url = `https://wa.me/${#contato}?text=${encodeURIComponent(mensagem)}`;
             window.open(url, '_blank')
 
             //reset
